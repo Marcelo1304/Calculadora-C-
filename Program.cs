cefa8f8eaaefa1eaa3203da506d3f6a0b0cc831c
Marcelo1304 <Marcelo_duarte13@outlook.com>
2023-10-09 22:23:12 -0300

    Calculadora

--- Program.cs
+++ Program.cs
@@ -2,7 +2,12 @@
 
 Calculadora calc = new Calculadora();
 
+calc.Potencia(3, 3);
 calc.Somar(25, 7);
 calc.Subtrair(25, 7);
 calc.Multiplicar(25, 7);
-calc.Dividir(2, 2);  
+calc.Dividir(2, 2);
+calc.Seno(30);
+calc.Coseno(30);
+calc.Tangente(30);
+calc.RaizQuadrada(9);
